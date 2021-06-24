1a035ee532838a03086dea920a49d12ed20d0ee9
yahia-raheem <eng.yahiaraheem.93@outlook.com>
2021-06-24 14:55:48 +0200

    0.2.1

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,1 +1,189 @@
-!function(e){var t={};function n(o){if(t[o])return t[o].exports;var r=t[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,n),r.l=!0,r.exports}n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)n.d(o,r,function(t){return e[t]}.bind(null,r));return o},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=1)}([function(e,t){var n=!1,o=document.getElementById("mySidenav");document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector(".navbar-toggler"),t=document.querySelector(".closebtn");document.querySelectorAll(".mobile-menu .menu-item").forEach((function(e){e.addEventListener("click",(function(t){if(t.stopPropagation(),e.classList.contains("menu-item-has-children")){var n=e.querySelector(".sub-menu"),o=e.querySelectorAll(".menu-item").length;n.classList.contains("opened")?(e.classList.remove("child-menu-opened"),n.classList.remove("opened"),n.style.maxHeight=0):(e.classList.add("child-menu-opened"),n.classList.add("opened"),n.style.maxHeight="".concat(64*o,"px"))}}))})),e.addEventListener("click",(function(){r(),document.addEventListener("click",c),setTimeout((function(){n=!0}),1e3)})),t.addEventListener("click",(function(){i()}))}));var r=function(){o.style.width="100vw"},i=function(){o.style.width="0",n=!1,u()},c=function(e){!o.contains(e.target)&&n&&i()},u=function(){document.removeEventListener("click",c)}},function(e,t,n){e.exports=n(2)},function(e,t,n){"use strict";n.r(t);n(0);document.addEventListener("DOMContentLoaded",(function(){}))}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/js/bundle.js":
+/*!**************************!*\
+  !*** ./src/js/bundle.js ***!
+  \**************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_sidebar__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/sidebar */ "./src/js/components/sidebar.js");
+/* harmony import */ var _components_sidebar__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_components_sidebar__WEBPACK_IMPORTED_MODULE_0__);
+// import "./components/bootstrap-imports";
+// import "./components/my-navbar";
+// import "./components/navigator";
+// import "./components/helpers";
+ // import './components/shared';
+
+document.addEventListener("DOMContentLoaded", function () {});
+
+/***/ }),
+
+/***/ "./src/js/components/sidebar.js":
+/*!**************************************!*\
+  !*** ./src/js/components/sidebar.js ***!
+  \**************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var isOpen = false;
+var sideBar = document.getElementById("mySidenav");
+document.addEventListener("DOMContentLoaded", function () {
+  var togglebtn = document.querySelector(".navbar-toggler");
+  var closebtn = document.querySelector(".closebtn");
+  var sidemenuItems = document.querySelectorAll(".mobile-menu .menu-item");
+  sidemenuItems.forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      e.stopPropagation();
+
+      if (item.classList.contains("menu-item-has-children")) {
+        var submenu = item.querySelector(".sub-menu");
+        var itemsNum = item.querySelectorAll(".menu-item").length;
+
+        if (submenu.classList.contains("opened")) {
+          item.classList.remove('child-menu-opened');
+          submenu.classList.remove("opened");
+          submenu.style.maxHeight = 0;
+        } else {
+          item.classList.add('child-menu-opened');
+          submenu.classList.add("opened");
+          submenu.style.maxHeight = "".concat(itemsNum * 64, "px");
+        }
+      }
+    });
+  });
+  togglebtn.addEventListener("click", function () {
+    openNav();
+    document.addEventListener("click", outsideClickListener);
+    setTimeout(function () {
+      isOpen = true;
+    }, 1000);
+  });
+  closebtn.addEventListener("click", function () {
+    closeNav();
+  });
+});
+
+var openNav = function openNav() {
+  sideBar.style.width = "100vw";
+};
+
+var closeNav = function closeNav() {
+  sideBar.style.width = "0";
+  isOpen = false;
+  removeClickListener();
+};
+
+var outsideClickListener = function outsideClickListener(event) {
+  if (!sideBar.contains(event.target) && isOpen) {
+    closeNav();
+  }
+};
+
+var removeClickListener = function removeClickListener() {
+  document.removeEventListener("click", outsideClickListener);
+};
+
+/***/ }),
+
+/***/ 0:
+/*!********************************!*\
+  !*** multi ./src/js/bundle.js ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /mnt/0CA504390CA50439/projects/deployed/starter-tailwind/src/js/bundle.js */"./src/js/bundle.js");
+
+
+/***/ })
+
+/******/ });
